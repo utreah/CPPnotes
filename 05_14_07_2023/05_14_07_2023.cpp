a90f53647387bce9d8e0353be5991a95028e9b72
utreah <91834152+utreah@users.noreply.github.com>
2024-10-14 11:27:52 +0100

    enum class, type cast giriş, unary scope resolution, ODR,  Inline func ve expansion,

--- 05_14_07_2023/05_14_07_2023.cpp
+++ 05_14_07_2023/05_14_07_2023.cpp
@@ -227,8 +227,111 @@ Istenmeyen durumlar;
 				//typecast operatörleri kullanýlarak bu dönüþümlerin yapýlmasý normal. typecast olmadan örtülü dönüþüm ile yapýldýðýnda bir problem ortaya çýktýðýnda
 					// problemi bulmanýn zor olduðundan bahsetti hoca.
 		}
-		1:50
+- Underlying-type: Underlying type, derleyiciye baðlý olduðu için incomplete type olarak kullanýlamýyor.
+	enum Color;
+	struct Engin{
+		//..
+		Color myvar; // bu structýn complete olabilmesi için Color türünün sizeof deðeri bilinmek zorunda.
+						// enum larýn türleri de underlying type olduðu için bu deðer herhangi bir þey olabilir.
+							// Color'ýn sizeof'u belli olmadýðý için burasý sentaks hatasý.
+	}
+
+- Scope kavramýndan dolayý sýkýntý olabiliyor. enum'ýn elemanlarýnýn scope'u, enum un tanýmýnýn yapýldýðý yerdir.
+	Yani içerisindeki elemanlarýn ayrý bir scopeu yok. enum global namespacede ise elemanlarý da ayný. Bundan dolayý
+		baþka bir enum elemaný veya baþka bir türün elemaný ile çakýþabilir, bu sentaks hatasý.
+engin.h
+	enum Color{White, Gray, Red}; 
+ali.h
+	enum ScreenColor{Magenta, Red, Black}; 
+Bu yukarýdaki iki .h dosyasýný ayný kaynak dosyasýna include ettiðimizi düþünelim. Color ve ScreenColor iki ayrý
+	identifier olsa da scopelarý ayný. Ayný zamanda enum'un elemanlarýnýn scopeuda ayný. Ýkisinde de Red, Black olduðu
+		için bu programý derlediðimizde derleyici sentaks hatasý verecek.
+
+Bu yukarýdaki sorunlarý çözmek için programcýlar bazý teknikler kullanýyorlardý.
+- enum adýný ve elemanlarýnýn adýnýn baþýna, sonuna belirleyici bir ek getiriyorlardý
+	enum TextWindowColor {TextWindowColorRed, TextWindowColorWhite}; 
+- Ayrý isim alaný(namespace) içine almak. Veya baþka sýnýflara nested olarak almak.
+
+Sorunlarý çözmek, kurallarý düzenlemek yerine yeni kural eklemiþler. avg Türkiye
+--------------------------------------------------------------------
+- Bildirimini yaparken enum'ýn underlying type'ýný kendimiz belirleyebiliyoruz. column tokenýný kullanarak.
+	Artýk ikinci probleme bir çözümümüz var. Struct artýk Color'ýn sizeof deðerini bileceðinden dolayý sentaks hatasý deðil.
+	enum Color : 'underlying-type' {Red, Green, Blue};
+	enum Color : unsigned int {Red, Green, Blue};
+
+- enum class diye yeni bir þey eklendi. Standart buna scoped enum(kapsamlandýrýlmýþ enum) diyor. 
+	Eski enum türünden bahsetmek istersek 'unscoped enum' veya 'traditional enum' diyebiliriz.
+	enum class ile beraber kapsamlandýrýlan enum'ýn kullaným þekli de deðiþti. Ayný zamanda artýk baþka .h dosylarýndan gelen
+		veya ayný kaynak dosyasýnda bulunan benzer enum elemanlarýnýn artýk çakýþma durumu ortadan kalktý. (239. satýr)
+
+	enum class Color : unsigned char {Green, Red, Blue};
+	int main(){
+		Color mycolor = Blue; // Bu sentaks hatasý. Kapsamýnda deðiliz. Burada scope resolution operatörünü binary
+								// infix olarak kullanmamýz gerekiyor.
+		Color mycolor = Color::Blue; // bu geçerli, scope resolution sayesinde eriþebiliyoruz
+	}
+----------------------------------------------------------------------------
+// traffic.h
+enum class TrafficLight {Red, Amber, Green};
+// widget.h
+enum class ScreenColor {White, Gray, Red, Blue, Black};		
+	int main(){
+		Trafficlight tl = TrafficLight::Red;
+		int x = tl; // eðer bu geleneksel enum olsa idi örtülü dönüþüm olacaðýndan dolayý legal olacaktý.
+						// fakat enum class bunun önüne geçiyor ve CPP gençlik hatasýný düzeltiyor. Hangimiz hata yapmadýk
+							// Eðer bilerek, isteyerek bu dönüþümü yapmak istiyorsak type-cast operatörü kullanabiliriz.
+		int z = static_cast<int>(tl); // type-castlerden bir tanesi. Daha görmedik.
+	
+	}
+-----------------------------------------------------------------------------
+- Scoped enum'ýn dile eklenmesi ile beraber bir çok problemi ortadan kaldýrsada baþka bir probleme sebep oldu.
+	Verbosity(V), Verbose(adj) diye bir kavram. Hoca bundan kod kalabalýðý olarak bahsetti. Kodun genel görünüþünün, okunuþunu zorlaþtýrdýðýný söylüyor.
+	
+	enum class Suit {Club, Diamond, Heart, Spade};
+
+	void func(Suit x){
+		Suit::Club; // bütün bildirimlerde, tanýmlarda böyle kullanýdlýðýný düþün. Çok fazla göz yoracak.
+						// bunun önüne geçmek için bir standart eklenmiþ
+		using enum Suit; // Bulunduðu scopetaki enumlarý nitelendirilmiþ enum olarak kullanma zorunluluðunu kaldýrýyor.
+		x = Club; // artýk Suit::Club yazmamýza gerek yok. C++20 ile eklendi.
+		auto y = Diamond; 
+	-------------------------------------------------------- Aþaðýdaki örnek için yukarýdaki using enum'ý görmezden gel
+	// Diyelim ki çok büyük bir enum class'ýmýz var. Fakat biz bunun içinden sadece 1-2 tanesini nitelendirilmemiþ
+		// olarak kullanmak istiyoruz.
+		using Suit::Diamond; // Bu bildirim ile, kendi scope'unda ilgili enumaratörün Diamond adlý elemanýný kullanabiliyoruz.
+		auto z = Diamond; // legal
+		auto x1 = Spade; // sentaks hatasý, Spade'e eriþemeyiz.
+	}
+*/
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+													/*					TYPE CAST OPERATORS					*/
+/*
+- (dönüþümün yapýlacaðý tür)deðiþkenin adý = (dval)int_expr;
+- 
+
+
 */
 
 
 
+/*
+int main(){
+	int x = 10;
+	const int* p = &x; // x'in kendisi const deðil, biz böyle tanýmlayarak p üzerinden deðiþiklik
+							// yapýlmasýný istemiyoruz.
+	// int* ptr = p; // böyle yazarsak hata verir çünkü const int* türünden int* türüne atama yapýlmaya çalýþýlýyor
+	int* ptr = (int*)p; // þimdi sýkýntý yok, hem de UB'de olmaz bu þekilde.  Normalde const int* olan bir nesnenin
+							// üzerinde deðiþiklik yapmak sýkýntý ama x'in kendisi const olmadýðý için sýkýntý yok.
+								// eðer const int x = 10; olsaydý UB vardý, 
+
+	// Kodun derleyici tarafýndaki sentaks hatasýný bu þekilde düzeltebiliriz. Fakat, bunu yaparken ayný zamanda
+		// yapýlan dönüþümün compatible olup olmadýðýna da bakmak gerekir.
+	int z = 12;
+
+	float* fz = (float*)(&z); // evet sentaks hatasý yok ama bu kod yalnýz. int ile float compatible type deðil.
+
+
+}	
+
+
+*/
