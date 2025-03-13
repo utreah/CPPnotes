2b763e506d6d70ca85bd3847e4ae03442986cbf4
utreah <91834152+utreah@users.noreply.github.com>
2025-03-13 14:14:38 +0000

    jh

--- 07_22_07_2024/07_22_07_2024.cpp
+++ 07_22_07_2024/07_22_07_2024.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-
-void foo(int* param) {
-	std::cout << "Value of param: " << *param << std::endl;
+void foo(int&& r) {
+	std::cout << "foo(int&& r)\n";
 }
-void foo(const int* param) {
-	std::cout << "Value of param: " << *param << std::endl;
+void foo(const int& r) {
+	std::cout << "foo(const int& r)\n";
+}
+void func(int&& r) {
+	foo(r);
 }
 
 int main(void) {
-	const int x{ 45 };
-	int y{ 66 };
-
-	foo(&x); // x const nesne ve const T* türünden T* türüne örtülü dönüþüm yok.
-	// bundan dolayý foo(int*) fonksiyonu viable bile deðil. const int* parametreli fonksiyon çaðrýlacak.
-	foo(&y); // burada her iki fonksiyonda viable olacak. Derleyici const olmayan parametreli fonksiyona
-	// öncelik tanýdýðý için int* parametreli fonksiyon çaðrýlacak
+	func(123);
 }
