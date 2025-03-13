25d6e7544406a28ce430423a4daf543250c97ea9
utreah <91834152+utreah@users.noreply.github.com>
2025-03-13 15:17:31 +0000

    derste hocanın mulakat sorusu dedigi sorular burada

--- mulakat.cpp
+++ mulakat.cpp
@@ -1,10 +1,55 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+	Dilde bir aracý öðrenirken þu sorularý sor
+1) Ne iþe yarýyor?
+2) Compile time / run time nerede çalýþýyor (mülakatlarda sorabiliyorlar bunu)
+*/
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+03_08_07_2024
+MÜLAKATLARDA HOCA BU SORUYU ÇOK SORUYORMUÞ
+	SORU: Aþaðýdaki kod çalýþtýrýldýðýnda ekrana yazdýrýlacak x'in deðeri ne olur?
+// c kodu;
+int main(){
+	int x = 5;
+	foo(x); // foo bir fonksiyon
+	printf("%d", x);
+}
+	CEVAP: Burada x nesnesi foo fonksiyonuna call by value ile gönderildiði için
+		foo içerisinde nasýl bir iþlem yapýlýrsa yapýlsýn x'in deðeri 5 olacak ve ekrana 5 yazdýrýlacak.
+			Eðer "foo'nun kodunu görmek gerek" derseniz 3152 kapýdan dýþarý.
+	Bu durum C için geçerli CPP için ise cevap "foo kodunu görmem gerek" Bunun sebebi
+		fonksiyon l value referans semantiði olarak kodlandý ise x'in deðeri deðiþir, eðer call by value ise
+			deðiþmez.
 
+int foo(int a){
+	a = 4;
+} // eðer foo böyle tanýmlandý ise bu call bu value, x deðeri deðiþmez.
 
+int foo(int& a){
+	a = 4;
+}// foo fonksiyonu l value referans semantiði ile tanýmlanmýþ x'in deðeri deðiþir.
+Özetle: 
+	C kodu için: Call by value olduðu için deðer deðiþmez.
+	CPP kodu için: Fonksiyonun tanýmý gerekli. 
+/------------------------------------------------------------------------------------------------------------------------/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+04_10_07_2024
+MÜLAKATTA ÇIKMA ÝHTÝMALÝ YÜKSEK BUNUN!
+int y = 10;
+decltype(y); L value yani int&
+decltype((y)); L value çünkü parantez içine alýnca ifade olarak ele alýnmasýný söylemiþ olduk yani L value oluyor 
+				ve o da int&
+-------------------------------------------------
 
+HATIRLATMA!
+Maximal Munch:Derleyici tokenizing yaparken en uzun tokený elde etmeye çalýþýyor.
+	whitespace karakterine kadar olan tokenlarý ayýra ayýra gidiyor. Yani
+
+	void foo(const char *= nullptr) burada *= ayrý bir operatör olarak iþlenir çünkü const chardan sonra
+		whitespace olduðu için const char olarak ele alýnýr.
+	void foo(const char* = nullptr) burada ise const char* default argument olarak nullptr alýyor.
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 07_22_07_2024.cpp
 MÜLAKAT SORUSU!!!
@@ -29,4 +74,8 @@ Ekrana const int& r parametreli fonksiyon yazar.func R value reference ile çaðrý
 foo ise r nesnesi ile yani L value reference ile çaðrýlýyor. const int& hem L hem R value ref alýyor.
 Burada foo'nun gönderdiði nesnenin Veri türü int&&. Referans kategorisi ile veri türü karýþtýrýlmamalý.
 r'nin int&& olmasý onun referans kategorisi ile alakasý yok. Bir nesnenin ismi her zaman L value reftir.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
