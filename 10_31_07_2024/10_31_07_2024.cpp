d974f5b49474dc1471b9b0aba78268a566065f3a
utreah <ersoyugur1528@gmail.com>
2025-08-05 01:32:27 +0100

    Constructors, Destructors, Incomplete Complete types, Special Member functions, Constructor Initializer List

--- 10_31_07_2024/10_31_07_2024.cpp
+++ 10_31_07_2024/10_31_07_2024.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <string>
-class Map {
-	// Derleyici tarafýndan implicitly-declared bir fonksiyon bildirilecek fakat bu fonksiyon sentaks hatasýna sebep olacka.
-		// çünkü int& bir l value reference ve sadece l value deðer alabilir. Default init edildiðinde garbage value verilecek
-			// yani verilen deðer r value olduðu için sentaks hatasý oluþacak ve bu yazýlan default constructor delete edilecek.
-private:
-	int& mx;
-};
+class Map;
+
+Map foo();
 int main(){}
